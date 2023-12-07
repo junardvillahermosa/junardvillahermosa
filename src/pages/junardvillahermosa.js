d1efe70660f96aa37b1e76d6f62114b8183e9b54
Junard Villahermosa <junard.villahermosa@rafi.ph>
2023-12-07 19:46:35 +0800

    feat(code): add-header

--- src/pages/junardvillahermosa.js
+++ src/pages/junardvillahermosa.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { styled } from "@mui/system";
-import { AppBar,Toolbar, Typography, Button, Box, Grid, Container, Card, CardContent } from "@mui/material";
+import { AppBar,Toolbar, Typography, Button, Box, Grid, Container, Card, CardContent, CardActions } from "@mui/material";
+import { Facebook, Twitter, GitHub, LinkedIn } from "@mui/icons-material";
+import ProfilePic from '.././images/junard.jpeg';
+
 
 const Header = styled(AppBar)(({theme}) => ({
-    backgroundColor: "#777777",
+    backgroundColor: "white",
     position: 'fixed',
 
 }));
@@ -18,38 +21,83 @@ const InnerHeader = styled(Toolbar)(({theme}) => ({
     fontWeight: "bold",
     '& a': {
         textDecoration: 'none',
-        color: 'white',
-        marginLeft: 30,
+        color: 'black',
+        marginLeft: 10,
+        padding: '10px 12px',
+        borderRadius: '5px',
     },
     '& .navigation': {
-        marginRight: 50,
+        marginRight: 30,
+        '& .home:hover': {
+            backgroundColor: 'black',
+            color: 'pink'
+        },
+        '& .about:hover': {
+            backgroundColor: 'black',
+            color: 'pink'
+        },
+        '& .projects:hover': {
+            backgroundColor: 'black',
+            color: 'pink'
+        },
+        '& .contact:hover': {
+            backgroundColor: 'black',
+            color: 'pink'
+        },
+
     },
     '& .junardlogo': {
         fontSize: 25,
         fontWeight: 'bold',
-        color: 'white',
+        color: 'black',
 
     }
 }));
 
 const FirstContainer = styled(Box)(({theme}) => ({
     position: 'absolute',
-    backgroundColor: 'whitesmoke',
+    backgroundColor: 'none',
     height: '800px',
     width: '100%',
 }));
 
 const MainContent = styled(Container)(({theme}) => ({
-    backgroundColor: 'white',
-    height: '300px',
-    width: '100%',
-    marginTop: '200px',
+    backgroundColor: 'none',
     display: 'flex',
+    alignItems: 'center',
+    height: 'auto',
+    width: '100%',
+    marginTop: '250px',
     padding: '30px',
     justifyContent: 'space-around',
     '& .content-here': {
         padding: '100px',
-    },
+    }
+
+}));
+
+const ButtonStyled = styled(Button)(({theme}) => ({
+    color: 'black',
+    padding: 10,
+    marginTop: 20,
+    fontSize: 20,
+    left: 0,
+    fontWeight: 'bold',
+    borderRadius: 5,
+    border: '3px solid pink',
+    '&:hover': {
+        backgroundColor: 'black',
+        border: '3px solid black',
+        color: 'pink'
+    }
+}));
+
+const ProfilePicture = styled('img')(({theme}) => ({
+    height: '400px',
+    width: '400px',
+    alignItems: 'center',
+    textAlign: 'center',
+    borderRadius: '500px',
 
 }));
 
@@ -61,44 +109,58 @@ export const JunardVillahermosa = () => {
     return (
         <React.Fragment>
             <Header>
-            <InnerHeader>
-                <a href="#Junard" className="junardvillahermosa">
-                    <Typography className="junardlogo">JUNARD VILLAHERMOSA</Typography>
-                </a>
-                <nav className="navigation">
-                    <a href="">Home</a>
-                    <a href="">About</a>
-                    <a href="">Contact</a>
-                </nav>
+                <InnerHeader>
+                    <a href="#Junard" className="junardvillahermosa">
+                        <Typography className="junardlogo">JUNARD VILLAHERMOSA</Typography>
+                    </a>
+                    <nav className="navigation">
+                        <a href="" className="home">HOME</a>
+                        <a href="" className="about">ABOUT</a>
+                        <a href="" className="projects">PROJECTS</a>
+                        <a href="" className="contact">CONTACT</a>
+                    </nav>
 
-            </InnerHeader>
-        </Header>
+                </InnerHeader>
+            </Header>
 
-        <FirstContainer>
-            <MainContent>
+            <FirstContainer>
+                <MainContent>
 
-                <Card sx={{ height: '200px', width: '300px'}}>
-                    <CardContent sx={{ backgroundColor: 'black',  height: '200px'}}>
+                    <Card sx={{ height: 'auto', width: '400px'}}>
+                        <CardContent sx={{ backgroundColor: 'transparent',  height: 'auto', border: 'none', boxShadow: 'none'}}>
+                            <Typography variant="h4" component='div' sx={{fontWeight: 'bold', fontSize: '50px'}}>
+                                Hello, World!
+                            </Typography>
+                            <Typography variant="h6" component='div' sx={{fontWeight: 'bold'}}>
+                                I am Junard D. Villahermosa
+                            </Typography>
+                            <Typography variant="h6" component='div'sx={{fontWeight: 'bold'}} >
+                                I am a future Software Engineer
+                            </Typography>
+                            <CardActions>
+                                <ButtonStyled>
+                                    Download Resume
+                                </ButtonStyled>
+                            </CardActions>
+                            <div style={{marginTop: '20px', display: 'flex', gap: '20px'}}>
+                                <a href=""><Facebook sx={{ color: 'blue', fontSize: '50px'}}/></a>
+                                <a href=""><Twitter sx={{ color: 'skyblue', fontSize: '50px'}}/></a>
+                                <a href=""><GitHub sx={{ color: 'black', fontSize: '50px'}}/></a>
+                                <a href=""><LinkedIn sx={{ color: 'blue', fontSize: '50px'}}/></a>
+                            </div>
 
-                    </CardContent>
 
-                </Card>
-                <Card sx={{height: '200px', width: '300px' }}>
-                    <CardContent sx={{ backgroundColor: 'red', height: '200px'}}>
+                        </CardContent>
 
-                    </CardContent>
+                    </Card>
+                    <Card sx={{height: 'auto', width: 'auto' }}>
+                        <CardContent sx={{ backgroundColor: 'white', height: 'auto'}}>
+                            <ProfilePicture src={ProfilePic}/>
 
-                </Card>
-
-
-
-            </MainContent>
-
-        </FirstContainer>
-
-
-
-
+                        </CardContent>
+                    </Card>
+                </MainContent>
+            </FirstContainer>
 
 
         </React.Fragment>
