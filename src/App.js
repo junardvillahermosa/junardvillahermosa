5126cf69d23611778ca8b8e8a65b5f2f1649128d
Junard Villahermosa <junard.villahermosa@rafi.ph>
2023-12-10 14:43:03 +0800

    update again

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { JunardVillahermosa } from "./pages/junardvillahermosa";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<JunardVillahermosa />}></Route>
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<JunardVillahermosa />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
